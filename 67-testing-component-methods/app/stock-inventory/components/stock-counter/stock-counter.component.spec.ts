d88f1145cdda1353e0358027fd154b2977bd47e3
Edson Machado <edson.machado@soprasteria.com>
2019-08-12 14:48:15 +0200

    testing components methods

--- 67-testing-component-methods/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
+++ 67-testing-component-methods/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
@@ -10,13 +10,49 @@ TestBed.initTestEnvironment(
 
 describe('StockCounterComponent', () => {
 
+  let component: StockCounterComponent;
+  let fixture: ComponentFixture<StockCounterComponent>;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         StockCounterComponent
       ]
-    });
+    })
 
-  });
+    fixture = TestBed.createComponent(StockCounterComponent);
+    component = fixture.componentInstance;
 
-});
+    component.value = 0;
+
+  })
+
+  it('should increment correctly', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+  })
+
+  it('should decrement correctly', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+    component.decrement();
+    expect(component.value).toBe(0);
+  })
+
+  it('should not decrement below the minimun value', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+    component.decrement();
+    expect(component.value).toBe(0);
+    component.decrement();
+    expect(component.value).toBe(0);
+  })
+
+  it('should not increment over the maximum value', () => {
+    for (let i = 0; i < 200; i++) {
+      component.increment();
+    }
+    expect(component.value).toBe(100);
+  })
+
+})
